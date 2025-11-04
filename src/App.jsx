2c90a43e334d13d13f56fd5679f9f4f6fa7f5d4d
muhammadimam156 <imamaliqadri5@gmail.com>
2025-11-05 00:51:21 +0500

    remove same files

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,10 @@ import Navbar from "./components/navbar/Navbar";
 import Slider from "./components/slider/Slider";
 import SpecialOffer from "./components/specialOffer/SpecialOffer";
 import Products from "./components/products/Products";
-import AdminProducts from "./pages/admin/products";
-import AdminLayout from "./components/admin/AdminLayout";
-import RequireAdmin from "./components/auth/RequireAdmin";
+
 
 import Shop from "./pages/Shop"; 
 import CustomerReviewApp from "./components/customerReviews/CustomerRewiews";
-import AdminDashboard from "./pages/admin/dashboard";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import Account from "./pages/Account.jsx";
-import Checkout from "./pages/Checkout.jsx";
 
 const HomePageContent = () => (
   <div>
@@ -34,15 +27,15 @@ function App() {
     <main>
     <Routes>
       <Route path="/" element={<HomePageContent />} />
-      <Route path="/admin" element={<RequireAdmin><AdminLayout /></RequireAdmin>}>
-      <Route index element={<AdminDashboard />} />
+      {/* <Route path="/admin" element={<AdminLayout />}> */}
+      {/* <Route index element={<AdminDashboard />} />
       <Route path="products" element={<AdminProducts />} />
-      </Route>
+      </Route> */}
       <Route path="/shop" element={<Shop />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/account" element={<Account />} />
-      <Route path="/checkout" element={<Checkout />} />
+      {/* <Route path="/login" element={<Login />} /> */}
+      {/* <Route path="/register" element={<Register />} /> */}
+      {/* <Route path="/account" element={<Account />} /> */}
+      {/* <Route path="/checkout" element={<Checkout />} /> */}
     </Routes>
     </main>
   </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 import Footer from './components/Footer/Footer.jsx';
-import { AuthProvider } from './context/AuthContext.jsx';
+// import { AuthProvider } from './context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AuthProvider>
+   
       <App />
       <Footer />
-    </AuthProvider>
+  
   </BrowserRouter>
 )
